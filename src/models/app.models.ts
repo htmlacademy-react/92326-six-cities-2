1742f1cb706c507ef555ddcf9420e92290e7e6a2
Tamo4ka <r.suu@yandex.ru>
2024-10-31 16:40:41 +0300

    add: моки

--- src/models/app.models.ts
+++ src/models/app.models.ts
@@ -1,3 +1,19 @@
 export interface Settings {
   placesToStayCount: number;
 }
+
+export enum OfferTypes {
+  Apartment = 'Apartment',
+  PrivateRoom = 'Private room',
+}
+
+export interface Offer {
+  id: number;
+  title: string;
+  preview: string;
+  isPremium: boolean;
+  isFavorite: boolean;
+  type: string;
+  rating: number;
+  price: number;
+}
