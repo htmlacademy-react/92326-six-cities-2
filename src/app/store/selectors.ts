403fe313b2ae798f169909b732bd90e287842cd1
Tamo4ka <r.suu@yandex.ru>
2024-11-29 12:45:13 +0300

    ref: каррирование

--- src/app/store/selectors.ts
+++ src/app/store/selectors.ts
@@ -1,13 +1,9 @@
 import { useAppSelector } from './hooks.ts';
 import { OfferItem } from '../../models/app.models.ts';
 
-export const useCitySelector = (): string => {
-  return useAppSelector((state) => state.city);
-};
+export const useCitySelector = (): string => useAppSelector((state) => state.city);
 
-export const useOffersSelector = (): OfferItem[] => {
-  return useAppSelector((state) => state.offers);
-};
+export const useOffersSelector = (): OfferItem[] => useAppSelector((state) => state.offers);
 
 export const useOfferListByCitySelector = (city: string): OfferItem[] => {
   const offers = useOffersSelector();
